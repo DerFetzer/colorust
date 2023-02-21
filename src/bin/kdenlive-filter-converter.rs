9d692850acdd778e65cbc186b331086c897e8dee
DerFetzer <kontakt@der-fetzer.de>
2023-02-21 23:29:56 +0100

    Add "insert into" file parameter

--- src/bin/kdenlive-filter-converter.rs
+++ src/bin/kdenlive-filter-converter.rs
@@ -10,10 +10,16 @@ use roxmltree::Document;
 #[command(author, version)]
 struct Cli {
     /// Kdenlive input file
+    /// (filters are extracted from here)
     input: PathBuf,
 
     /// Output file
     output: PathBuf,
+
+    /// Optional file where the filtergraph strings are inserted
+    /// (if not set the input file is used)
+    #[arg(short, long)]
+    insert_into: Option<PathBuf>,
 
     /// Delete existing filtergraph properties from all producers
     #[arg(short, long)]
@@ -31,8 +37,21 @@ fn main() -> Result<()> {
     let filter_strings = get_filter_strings(&doc.root());
     debug!("Filter strings: {filter_strings:#?}");
 
-    let mlt_out =
-        add_filtergraph_to_producers(mlt, &filter_strings, cli.delete_existing_filtergraph);
+    let insert_into = cli
+        .insert_into
+        .map(|p| std::fs::read_to_string(p).wrap_err("Could not read insert_into file"));
+
+    let insert_into = match insert_into {
+        None => mlt,
+        Some(Ok(insert_into)) => insert_into,
+        Some(Err(e)) => return Err(e),
+    };
+
+    let mlt_out = add_filtergraph_to_producers(
+        insert_into,
+        &filter_strings,
+        cli.delete_existing_filtergraph,
+    );
     std::fs::write(cli.output, mlt_out).wrap_err("Could not write output file")?;
 
     Ok(())
